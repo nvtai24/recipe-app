5654627717f66c8ee52adde2cd2852fbe147fdb0
Nvtai <buiblue123@gmail.com>
2025-05-25 12:02:16 +0700

    feat: update styling for IngredientItem and recipe instructions

--- src/components/IngredientItem.jsx
+++ src/components/IngredientItem.jsx
@@ -2,15 +2,20 @@ import styles from "./IngredientItem.module.css";
 
 export default function IngredientItem({ ingredient }) {
   return (
-    <div>
-      <img
-        src={`https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}`}
-        alt={ingredient.name}
-      />
-      <h3>{ingredient.name}</h3>
-      <h3>
-        {ingredient.amount} {ingredient.unit}
-      </h3>
+    <div className={styles.itemContainer}>
+      <div className={styles.imageContainer}>
+        <img
+          src={`https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}`}
+          alt={ingredient.name}
+        />
+      </div>
+
+      <div className={styles.textContainer}>
+        <div className={styles.itemName}>{ingredient.name}</div>
+        <div className={styles.itemAmount}>
+          {ingredient.amount} {ingredient.unit}
+        </div>
+      </div>
     </div>
   );
 }
